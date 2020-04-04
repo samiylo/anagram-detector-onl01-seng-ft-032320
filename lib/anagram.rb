e7bfa9af17577cb392bd428b84908d1c85b0b49c
Samiylo Kryshu <skryshu@gmail.com>
2020-04-04 22:14:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,4 +5,12 @@ class Anagram
   def initialize(word)
     @word = word
   end
+  
+  def match(array_of_words)
+
+    
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
 end
